a4322309dacae2329c39d7cbeaf6a43e4db8a4e9
YoussouphNdiour <83768182+YoussouphNdiour@users.noreply.github.com>
2023-08-02 12:30:19 +0000

    Update otpCodeInput.tsx

--- components/inputs/otpCodeInput.tsx
+++ components/inputs/otpCodeInput.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import { OtpInputProps } from "react-otp-input";
 
-export default function OtpCodeInput(props: OtpInputProps) {
+export default function OtpCodeInput() {
    return <div>a</div> //<OtpInput {...props} inputStyle={{ width: 83, height: 83 }} />;
 }
