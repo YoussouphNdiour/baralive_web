71112de2f1cdaa69e8de793cf354bc2a61ec7e68
YoussouphNdiour <83768182+YoussouphNdiour@users.noreply.github.com>
2023-08-02 12:52:29 +0000

    Update verifyCodeForm.tsx

--- components/verifyCodeForm/verifyCodeForm.tsx
+++ components/verifyCodeForm/verifyCodeForm.tsx
@@ -3,7 +3,7 @@ import cls from "./verifyCodeForm.module.scss";
 import { useTranslation } from "react-i18next";
 import PrimaryButton from "components/button/primaryButton";
 import { useFormik } from "formik";
-import OtpCodeInput from "components/inputs/otpCodeInput";
+ /import OtpCodeInput from "components/inputs/otpCodeInput";
 import { useRouter } from "next/router";
 import authService from "services/auth";
 import { error, success } from "components/alert/toast";
@@ -89,14 +89,14 @@ export default function VerifyCodeForm({}: Props) {
       </div>
 
       <div className={cls.space} />
-      <OtpCodeInput
+{/*       <OtpCodeInput
         value={formik.values.code}
         onChange={(otp: string) => formik.setFieldValue("code", otp)}
         numInputs={6}
         isInputNum
         containerStyle={cls.otpContainer}
         hasErrored={!!formik.errors.code}
-      />
+      /> */}
       <p className={cls.text}>
         {t("verify.didntRecieveCode")}{" "}
         {time === 0 ? (
